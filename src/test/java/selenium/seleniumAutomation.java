1405a625213e28b0a57d487a6585784c9d65cb3b
sudhanshu <sudhanshu2354@gmail.com>
2026-09-02 07:36:56 +0530

    Architect us commit

--- src/test/java/selenium/seleniumAutomation.java
+++ src/test/java/selenium/seleniumAutomation.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 		public void seleniumTest1() {
 			System.out.println("hello selenium");
 			System.out.println("change by us person");
+			System.out.println("changes in develop branch");
 		}
 		
 		
